be9df1920828e8cd2c7ba9c68753fb1b3a9b87eb
melikaBagherii <m.bagheri1029@gmail.com>
2024-09-01 14:38:04 +0330

    chore: add isNegative and sort methods

--- exercises/js array methods/arrayMethods.js
+++ exercises/js array methods/arrayMethods.js
@@ -1,6 +1,8 @@
 numbers1 = [1,2,3,4,5,6,7,8,9,10]
 numbers2 = [1,2,3,4,5]
 numbers3 = [1,2,3,4,5,6,7,8]
+mixedNumbers = [1,-2,3,4,-5]
+fruits = ['banana','apple','orange','mango']
 
 function evenNumbers(numbers){
     evenNumbers = []
@@ -29,10 +31,10 @@ function sum(numbers){
 
 
 function greaterThan(numbers, base){
-    firstNumberGreaterThanBase = 0
+    firstNumberGreaterThanBase = []
     for(let i=0; i<numbers.length; i++){
         if(numbers[i]>base){
-            firstNumberGreaterThanBase = numbers[i]
+            firstNumberGreaterThanBase.push(numbers[i])
             break
         }
     }
@@ -40,10 +42,20 @@ function greaterThan(numbers, base){
 }
 
 
-function
+function isNegative(numbers){
+    for(let i=0; i<numbers.length; i++){
+        if(numbers[i]<0){
+            return true
+        }
+    }
+    return false
+}
 
 
 document.getElementById("evenNumbers").innerHTML = '[' + evenNumbers(numbers1) + ']'
 document.getElementById("squaredNumbers").innerHTML = '[' + square(numbers2) + ']'
 document.getElementById("ArrayElementSum").innerHTML = sum(numbers2)
 document.getElementById("greaterThanFive").innerHTML = greaterThan(numbers3,5)
+document.getElementById("isNegative").innerHTML = isNegative(mixedNumbers) ? 'There is Negative Number in this Array!': 'There is No Negative Number in this Array!'
+document.getElementById("allPositive").innerHTML = isNegative(numbers2) ?'There are Negative Elements in this Array':'All Elements in this Array are Positive!'
+document.getElementById("stringSort").innerHTML = fruits.sort()
