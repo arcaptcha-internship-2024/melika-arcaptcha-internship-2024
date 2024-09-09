8029acbd325fd0ec2b21eb09553a1abd09bc27f6
melikaBagherii <m.bagheri1029@gmail.com>
2024-09-09 21:03:01 +0330

    Add DeleteItem method

--- exercises/crud/nodejsCrud.js
+++ exercises/crud/nodejsCrud.js
@@ -1,47 +1,47 @@
 var http = require('http')
 
 let database = []
+
 function create(){
     let userArray = ['test']
     database.push(userArray)
     return userArray
 }
 
+
 function read(){
     return database[0]
 }
 
+
 function update(item,index=-1){
     array = database[0]
-    if(index == -1){
-        array.push(item)
-    }
-    else{
-        array[index] = item
-    }
+    index == -1 ? array.push(item) : array[index] = item
     return array
-    
+}
+
+
+function deleteItem(){
+    array = database[0]
+    array.pop()
+    return array
 }
 
 
 var server = http.createServer(function(req, res){
     let body = ''
     let url = req.url
-    // console.log(body.length)
+
     req.on('data', chunk =>{
-        console.log('you have recieved data!!!!!!')
         body += chunk.toString()
     })
 
     req.on('end',()=>{
-        
         let parsedBody = {};
         
-    // Parse the body only if it is not empty
         if (body.trim()) {
-            // console.log(`this is your fucking body: ${body.length}`)
-            console.log(body)
-            parsedBody = JSON.parse(body);}
+            parsedBody = JSON.parse(body);
+        }
         
         switch (url) {
             case '/create':
@@ -49,10 +49,12 @@ var server = http.createServer(function(req, res){
                 console.log("your array is created!!" + createdArray)
                 res.end("your array is created!!")
                 break;
+
             case '/read':
                 let readArray = read()
                 res.end(`this is your array : [${readArray}]`)
                 break;
+
             case '/update':
                 let updatedArray;
                 if(parsedBody.hasOwnProperty("index")){
@@ -60,12 +62,14 @@ var server = http.createServer(function(req, res){
                 }else{
                     updatedArray = update(parsedBody.item)
                 }
-                
                 res.end(`this is your array : [${updatedArray}]`)
                 break;
+
             case '/delete':
-                res.end('you are in delete page')
+                let deletedArray = deleteItem()
+                res.end(`this is your array : [${deletedArray}]`)
                 break;
+
             default:
                 res.end('use create, read, update and delete paths')
                 break;
