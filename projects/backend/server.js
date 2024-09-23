bc7a671ced8e269c7819ec7849413e570a81a647
melikaBagherii <m.bagheri1029@gmail.com>
2024-09-23 17:41:47 +0330

    fix: set the host to give access to docker

--- projects/backend/server.js
+++ projects/backend/server.js
@@ -7,13 +7,14 @@ fastify.register(require('./routes/routes'))
 fastify.register(require('@fastify/static'), {
     root: path.join(__dirname, '../frontend'),
     prefix: '/',
+    // origin: 'http://localhost:8080'
   });
 
 fastify.register(require('@fastify/formbody'));
 
 const start = async () => {
     try {
-      await fastify.listen({ port: 3000 })
+      await fastify.listen({port: 3000,  host:'0.0.0.0'})
     } catch (err) {
       fastify.log.error(err)
       process.exit(1)
