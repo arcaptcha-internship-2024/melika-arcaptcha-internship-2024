985614fc9d46461d048c59cb82aca63f28d0153d
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-07 15:11:49 +0330

    chore: use dynamic classes v-for

--- lessons/vue/app.js
+++ lessons/vue/app.js
@@ -2,15 +2,25 @@ const app = Vue.createApp(
     {
         data(){
             return{
-                title: 'The Final Empire',
-                author: 'Brandon Sanderson',
-                age: 45,
+                books:[
+                    {title: 'name of the wind', author: 'patrick rothfuss', isFav: true, img: 'assets/1.jpeg'},
+                    {title: 'the way of kings', author: 'brandon sanderson', isFav: false, img: 'assets/2.jpeg'},
+                    {title: 'the final empire', author: 'brandon sanderson', isFav: true, img: 'assets/3.jpeg'}
+                ],
                 showBooks: false
             }
         },
         methods:{
             toggleShowBooks(){
                 this.showBooks = !this.showBooks
+            },
+            toggleFave(book){
+                book.isFav = !book.isFav
+            }
+        },
+        computed:{
+            filteredBooks(){
+                return this.books.filter((book)=>book.isFav)
             }
         }
     }
