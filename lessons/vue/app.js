0c0448fa0f0efdf7d94e305cc5ba339023df3f9c
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-07 13:17:22 +0330

    chore: using v-if, v-else and v-show

--- lessons/vue/app.js
+++ lessons/vue/app.js
@@ -4,14 +4,13 @@ const app = Vue.createApp(
             return{
                 title: 'The Final Empire',
                 author: 'Brandon Sanderson',
-                age: 45
+                age: 45,
+                showBooks: false
             }
         },
         methods:{
-            changeTitle(title){
-                // this.title = 'Words of Randiance'
-                this.title = title
-
+            toggleShowBooks(){
+                this.showBooks = !this.showBooks
             }
         }
     }
