1e5d7528bfc7c2363d26bf146980e8bd5609b8b0
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-09 01:15:47 +0330

    feat: add reaction-timer project

---
+++ lessons/vue/courseProject/timerProjext/reaction-timer/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1>Melika Reaction Timer</h1>
+    <button @click="start" v-bind:disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying" v-bind:delay="delay" @end="endGame"/>
+    <Result v-if="showResults" v-bind:score="score"/>
+  </div>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
+export default {
+  name: 'App',
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  components: {Block, Result},
+  methods:{
+    start(){
+      this.showResults = false
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 5000
+    },
+    endGame(reactionTime){
+      this.showResults = true
+      this.score = reactionTime
+      this.isPlaying = false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button{
+  background-color: rgb(37, 148, 24);
+  color: white;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
