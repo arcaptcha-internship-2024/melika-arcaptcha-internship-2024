a2927a8572fcf0c3db6c5e6e5cae6fb6fdd20309
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-08 19:25:44 +0330

    feat: add course project

---
+++ lessons/vue/courseProject/myfirstproject/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>{{title}}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click</button>
+  <Modal />
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  name: 'App',
+  data(){
+    return{
+      title: 'My First Vue App :)'
+    }
+  },
+  components:{
+    Modal
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lessons/vue/courseProject/myfirstproject/src/components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <h1>Modal Title</h1>
+            <p>This is a Modal</p>
+        </div>
+    </div>
+</template>
+
+<style>
+    .modal{
+        width: 400px;
+        margin: 100px auto;
+        background: white;
+        padding: 20px;
+        border-radius: 10px;
+    }
+    .backdrop{
+        background: rgba(0, 0, 0, 0.5);
+        top: 0;
+        position: fixed;
+        width: 100%;
+        height: 100%;
+    }
+    .modal h1{
+        color: rgb(218, 81, 18);
+        border: none;
+        padding: 0;
+    }
+</style>
