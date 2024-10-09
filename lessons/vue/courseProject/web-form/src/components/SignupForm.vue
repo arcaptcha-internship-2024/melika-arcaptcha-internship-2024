a3554e10d0e5cf4d78ee26aaa809d724573f37f7
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-09 13:54:59 +0330

    chore: form submission and password validation

--- lessons/vue/courseProject/web-form/src/components/SignupForm.vue
+++ lessons/vue/courseProject/web-form/src/components/SignupForm.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-    <form>
+    <form @submit.prevent="handleSubmit">
         <label>Email:</label>
         <input type="email" required v-model="email">
 
         <label>Password:</label>
         <input type="password" required v-model="password">
-
+        <p v-if="passwordError" class="error">{{this.passwordError}}</p>
         <label>Role:</label>
         <select v-model="role">
             <option value="developer">Web Developer</option>
             <option value="designer">Web Designer</option>
         </select>
-        <div>
-            <input type="checkbox" v-model="terms" required>
-            <label>Accept terms and conditions</label>
-        </div>
         <label>Skills:</label>
         <input type="text" v-model="tempSkill" @keyup.ctrl="addSkill">
         <div v-for="skill in skills" :key="skill" class="pill">
             <span @click="deleteSkill(skill)">{{skill}}</span>
+        </div>
+        <div>
+            <input type="checkbox" v-model="terms" required>
+            <label>Accept terms and conditions</label>
+        </div>
+        
+        <div class="submit">
+            <button>Create an Account</button>
         </div>
     </form>
     <p>Email: {{email}}</p>
@@ -33,12 +37,13 @@
 export default {
     data(){
         return{
-            email:'melika',
+            email:'melika@gmail.com',
             password:'1234',
             role: 'designer',
             terms: false,
             tempSkill: '',
-            skills:[]
+            skills:[],
+            passwordError: ''
         }
     },
     methods: {
@@ -54,6 +59,19 @@ export default {
             this.skills = this.skills.filter((item)=>{
                 return skill !== item
             })
+        },
+        handleSubmit(){
+            this.passwordError = ''
+            this.passwordError = this.password.length > 5 ? '': 'Password length must be at least 6 characters long'
+
+            if(!this.passwordError){
+                console.log('Email:',this.email)
+                console.log('Password:',this.password)
+                console.log('Role:',this.role)
+                console.log('Terms Accepted:',this.terms)
+                console.log('Skills:',this.skills)
+
+            }
         }
     }
 }
@@ -104,5 +122,22 @@ input[type="checkbox"]{
     font-weight: bold;
     color: #777;
     cursor: pointer;
+}
+button{
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 30px;
+    color: white;
+    border-radius: 20px;
+}
+.submit{
+    text-align: center;
+}
+.error{
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
 }
 </style>
