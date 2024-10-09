72adc9beb046589ac55750edb4bad91e5253af40
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-09 15:48:19 +0330

    chore: use route parameters

--- lessons/vue/courseProject/ninja-jobs/src/router/index.js
+++ lessons/vue/courseProject/ninja-jobs/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import Jobs from '../views/jobs/Jobs.vue'
+import JobDetail from '../views/jobs/JobDetail.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/jobs',
     name: 'jobs',
     component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'jobDetail',
+    component: JobDetail
   }
 ]
 
---
+++ lessons/vue/courseProject/ninja-jobs/src/views/jobs/JobDetail.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Job Details Page</h1> 
+        <p>The job id is: {{id}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            id: this.$route.params.id
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
