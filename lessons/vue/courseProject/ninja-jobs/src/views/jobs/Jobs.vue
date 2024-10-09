d6de54cda8ceb5afa02ec7f0be05e5f9fb71a2d5
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-09 15:38:06 +0330

    chore: use routers

---
+++ lessons/vue/courseProject/ninja-jobs/src/views/jobs/Jobs.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Jobs</h1>
+        <div v-for="job in jobs" :key="job.id">
+            {{job.title}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            jobs: [
+                {title: 'Ninja Vue Developer', id: 1, detail:'nothing'},
+                {title: 'Ninja UX Designer', id: 1, detail:'nothing'},
+                {title: 'Ninja Web Developer', id: 1, detail:'nothing'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
