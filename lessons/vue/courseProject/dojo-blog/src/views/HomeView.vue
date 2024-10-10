6c77f4c32d5c6c3e22993036871e011b51f1b1e9
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-10 15:25:29 +0330

    feat: use composition api and refs

---
+++ lessons/vue/courseProject/dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p ref="p">My name is {{name}} and I'm {{age}} years old!</p>
+    <button @click="handleClick">Click me</button>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name: 'HomeView',
+  setup(){
+    let age = 23
+    let name = 'Melika'
+    let p = ref(null)
+
+    const handleClick = () => {
+      console.log('You clicked me!')
+      console.log(p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'Hello Melika!!'
+    }
+    return{age, name, handleClick, p}
+  }
+}
+</script>
