8ca17b1ccb63849b40aaa80994258f44689c07f9
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-10 15:51:27 +0330

    chore: use reactive to create reactive variables

--- lessons/vue/courseProject/dojo-blog/src/views/HomeView.vue
+++ lessons/vue/courseProject/dojo-blog/src/views/HomeView.vue
@@ -1,26 +1,34 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>My name is {{name}} and I'm {{age}} years old!</p>
-    <button @click="handleClick">Click me</button>
-    <input type="text" v-model="name">
-    <button @click="age++">Increase Age</button>
+    <h2>Refs</h2>
+    <p>My name is {{ninjaOne.name}} and I'm {{ninjaOne.age}} years old!</p>
+    <button @click="updateNinjaOne">Update Ninja one</button>
+    
+    <h2>Reactive</h2>
+    <p>{{ninjaTwo.name}} - {{ninjaTwo.age}}</p>
+    <button @click="updateNinjaTwo">Update Ninja two</button>
   </div>
 </template>
 
 <script>
-import {ref} from 'vue'
+import {reactive, ref} from 'vue'
 export default {
   name: 'HomeView',
   setup(){
-    const age = ref(23)
-    const name = ref('Melika')
+    const ninjaOne = ref({name: 'Melika', age: 23})
+    const ninjaTwo = reactive({name: 'Nima', age: 21})
 
-    const handleClick = () => {
-      name.value = 'Nima'
-      age.value = 21
+    //this is a primitive value and you can't change it  it's better to use refs all the time
+    const name = reactive('Rojin')
+
+    const updateNinjaOne = () => {
+      ninjaOne.value.age += 10
     }
-    return{age, name, handleClick}
+    const updateNinjaTwo = () => {
+      ninjaTwo.age += 10
+    }
+    return{ninjaOne, updateNinjaOne, updateNinjaTwo, ninjaTwo}
   }
 }
 </script>
