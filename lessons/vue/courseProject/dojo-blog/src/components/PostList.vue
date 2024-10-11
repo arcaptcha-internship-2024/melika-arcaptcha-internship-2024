67ab00c390b6a8c6517d74bce23dde29aa7a7b04
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-12 00:37:15 +0330

    chore: use props in composition api

---
+++ lessons/vue/courseProject/dojo-blog/src/components/PostList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="post-list"> 
+    <div v-for="post in posts" :key="post">
+        <Post :post="post"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Post from '../components/Post.vue'
+import {computed} from 'vue'
+export default {
+
+    props: ['posts'],
+    components:{Post},
+}
+</script>
+
+<style>
+
+</style>
--- lessons/vue/courseProject/dojo-blog/src/views/HomeView.vue
+++ lessons/vue/courseProject/dojo-blog/src/views/HomeView.vue
@@ -1,38 +1,23 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>search term: {{search}}</p>
-    <div v-for="name in matchedNames" :key="name">
-      {{name}}
-    </div>
-    <button @click="handleClick">Stop</button>
+    <PostList :posts="posts"/>
   </div>
 </template>
 
 <script>
+import PostList from "../components/PostList.vue"
 import {computed, reactive, ref, watch, watchEffect} from 'vue'
+
 export default {
   name: 'HomeView',
+  components: {PostList},
   setup(){
-    const search = ref('')
-    const names = ref(['melika','nima','rojin','maryam','ali','fatemeh','mojan','sara'])
-
-    const stopWatch = watch(search, () => {
-      console.log('watch function ran')
-    })
-    const stopEffect = watchEffect(() => {
-      console.log('watchEffect function ran!',search.value)
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
-    const matchedNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-    return{search,names,matchedNames,handleClick}
+    const posts = ref([
+      {title: 'Welcome to the Blog', id: 1, body:'Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum'},
+      {title: 'Top 5 CSS tips', id: 2, body:'Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum'},
+    ])
+    return{posts}
   }
 }
 </script>
