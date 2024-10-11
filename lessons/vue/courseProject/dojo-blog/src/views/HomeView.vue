1d5efde29a0d5f8a26b208ab594ae510a756e65a
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-11 15:09:10 +0330

    chore: use watch and watchEffect functions

--- lessons/vue/courseProject/dojo-blog/src/views/HomeView.vue
+++ lessons/vue/courseProject/dojo-blog/src/views/HomeView.vue
@@ -6,20 +6,33 @@
     <div v-for="name in matchedNames" :key="name">
       {{name}}
     </div>
+    <button @click="handleClick">Stop</button>
   </div>
 </template>
 
 <script>
-import {computed, reactive, ref} from 'vue'
+import {computed, reactive, ref, watch, watchEffect} from 'vue'
 export default {
   name: 'HomeView',
   setup(){
     const search = ref('')
     const names = ref(['melika','nima','rojin','maryam','ali','fatemeh','mojan','sara'])
+
+    const stopWatch = watch(search, () => {
+      console.log('watch function ran')
+    })
+    const stopEffect = watchEffect(() => {
+      console.log('watchEffect function ran!',search.value)
+    })
+
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
     const matchedNames = computed(() => {
       return names.value.filter((name) => name.includes(search.value))
     })
-    return{search,names,matchedNames}
+    return{search,names,matchedNames,handleClick}
   }
 }
 </script>
