839a2cf0d328c0328d65f5be3fba6d1eb61b702c
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-13 20:40:41 +0330

    chore: add arcaptch to the form and handle submission

--- projects/frontend/my-frontend-vue/src/components/MyForm.vue
+++ projects/frontend/my-frontend-vue/src/components/MyForm.vue
@@ -1,34 +1,74 @@
 <template>
-  <form id="myForm">
+  <form id="myForm" action="http://localhost:3000" enctype="application/x-www-form-urlencoded" method="POST" @submit.prevent="handleSubmit">
     <h1>Submit Your Details</h1>
     <div v-for="formField in formFields" :key="formField.id">
       <InputGroup :id="formField.id" :type="formField.type" :placeholder="formField.placeholder" :label="formField.label" :isRequired="formField.isRequired"/>
     </div>
     <label for="explanation">Explanation:</label>
     <textarea id="explanation" name="explanation" rows="4" placeholder="Provide a short explanation" required></textarea>
-    <div class="arcaptcha" data-site-key="qh7aotm3n8"></div>
-    
+    <arcaptchaVue3 :callback="callbackDef" :expired_callback="expired_callbackDef" site_key="qh7aotm3n8">
+    </arcaptchaVue3>
+
     <button type="submit">Submit</button>
   </form>
 </template>
 
 <script>
+import arcaptchaVue3, { data } from "arcaptcha-vue3";
+import {ref} from 'vue'
 import InputGroup from './InputGroup.vue'
 export default {
   components: {
-    InputGroup
+    InputGroup,
+    arcaptchaVue3
   },
   setup(){
+    const widget = ref(null)
     const formFields = [
       {id: "name", type: "text", placeholder: "Your full name", label: "Name: ", isRequired: true},
       {id: "companyName", type: "text", placeholder: "Company you work for", label: "Company Name: ", isRequired: true},
       {id: "jobPosition", type: "text", placeholder: "Your job position", label: "Job Position: ", isRequired: true},
       {id: "phoneNumber", type: "tel", placeholder: "Your phone number", label: "Phone Number: ", isRequired: true}
     ]
-    return{formFields}
+    
+    const handleSubmit = async () => {
+      const form = document.querySelector('form')
+      const formData = new FormData(form);
+      const urlEncoded = new URLSearchParams(formData).toString();
+
+      fetch('http://localhost:3000/upload',{
+        method: "POST",
+        body: urlEncoded, // just 'fd' for multipart/form-data
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(response => response.json()).then(
+        data => {
+          if (data.success){
+            alert(data.message)
+            document.getElementById("myForm").reset()
+            window.arcaptcha.reset()
+          }else{
+            alert(data.message)
+          }
+        }
+      )
+    }
+    return{formFields, handleSubmit, widget}
+  },
+  methods: {
+    expired_callbackDef() {
+        //This method would be called after expiring
+    },
+    callbackDef() {
+        // This method would be called after solving captcha
+    }
   }
 }
 </script>
+
+
+
 <style>
 form {
   background-color: #fff;
