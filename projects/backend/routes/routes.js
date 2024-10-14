c119f0d06d997d236a3bf035d282d788d8c708ac
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-14 19:09:53 +0330

    chore: register routes

--- projects/backend/routes/routes.js
+++ projects/backend/routes/routes.js
@@ -1,8 +1,9 @@
 const {saveUserData} = require('../controllers/operations')
-
+const {login} = require('../controllers/operations')
 function routes(fastify,options,done){
     fastify.register(require('@fastify/formbody'));
     fastify.post('/upload',saveUserData)
+    fastify.post('/login',login)
     done()
 }
 
