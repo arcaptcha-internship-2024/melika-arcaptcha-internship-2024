c052d75fc5e2fe60a51df88bccdfa6075e76f4a6
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-16 03:11:11 +0330

    chore: register AdminView and SalesManagerView components

--- projects/frontend/my-frontend-vue/src/router/index.js
+++ projects/frontend/my-frontend-vue/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+import AdminView from '../views/AdminView.vue'
+import SalesManagerView from '../views/SalesManagerView.vue'
 
 const routes = [
   {
@@ -12,6 +14,16 @@ const routes = [
     path: '/login',
     name: 'login',
     component: LoginView
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: AdminView
+  },
+  {
+    path: '/salesManager',
+    name: 'salesManager',
+    component: SalesManagerView
   }
 ]
 
