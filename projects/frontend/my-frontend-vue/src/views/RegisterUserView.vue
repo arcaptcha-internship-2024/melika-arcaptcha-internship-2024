aa8a6418e0cd942cd67abd4e9a0d73be7c4d362c
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-21 17:27:05 +0330

    refactor: create a saparate view for user registry with its own path

---
+++ projects/frontend/my-frontend-vue/src/views/RegisterUserView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="register-form">
+        <UserForm :buttonContent="buttonContent" :headerContent="headerContent" />
+    </div>
+</template>
+
+<script>
+import UserForm from '../components/UserForm.vue'
+export default {
+    name:'RegisterUserView',
+    components:{
+        UserForm
+    },
+    setup(){
+        const headerContent = "Register User"
+        const buttonContent = "Register"
+        return{headerContent, buttonContent}
+    }
+}
+</script>
+
+<style>
+.register-form {
+    max-width: 400px;
+    width: 100%;
+    display: flex;
+    justify-content: center; /* Horizontally center the form */
+    align-items: center; /* Vertically center the form */
+    height: 90vh;
+    margin: 0 auto; 
+}
+
+</style>
