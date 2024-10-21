73cf750209797e51ee8c8dc8b1559560fc1bf807
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-21 17:26:03 +0330

    refactor: separate userForm from loginForm

---
+++ projects/frontend/my-frontend-vue/src/components/UserForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="user-form">
+        <MyForm :formFields="userFields" class="form-card" :buttonContent="buttonContent" :headerContent="headerContent" :multiRole="multiRole" :selectInfo="selectInfo"/>
+    </div>
+</template>
+
+<script>
+import MyForm from '../components/MyForm.vue'
+
+export default {
+    name:'UserForm',
+    components:{
+        MyForm
+    },
+    props:['headerContent','buttonContent'],
+    setup(props){
+        const userFields = [
+            {id: "email", type: "email", rows: "", placeholder: "Your email", label: "Email: ", isRequired: true, fieldType:"input"},
+            {id: "password", type: "password", rows: "", placeholder: "Your password", label: "Password: ", isRequired: true, fieldType:"input"},
+        ]
+        const headerContent = props.headerContent
+        const buttonContent = props.buttonContent
+        const multiRole = true
+        const selectInfo = {
+            id:"role",
+            label: "Select Role: ",
+            options: [
+                {value: "default",content: "Select Role", isDisabled: true},
+                {value: "admin",content: "Admin", isDisabled: false},
+                {value: "sales_manager",content: "Sales Manager", isDisabled: false}
+            ]
+        }
+        return {userFields, headerContent, buttonContent, multiRole, selectInfo}
+    }
+}
+</script>
+
+<style>
+.user-form{
+  width: 100%;
+}
+</style>
