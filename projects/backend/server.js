32688fdc15b04c10fb36da4a98ffb97a539d2ac6
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-21 13:16:52 +0330

    chore: set secret key for jwt

--- projects/backend/server.js
+++ projects/backend/server.js
@@ -13,9 +13,21 @@ fastify.register(require('@fastify/cors'), {
   // origin: 'http://localhost:8080'
   origin: '*'
 });
-
+require("./hooks/jwt.js")(fastify);
+require('dotenv').config();
 fastify.register(require('@fastify/formbody'));
+fastify.register(require('@fastify/jwt'), {
+  // secret: 'supersecret'  // Replace 'supersecret' with an environment variable for production
+  secret: process.env.SECRET_KEY
+});
 
+// fastify.decorate('authenticate', async function (request, reply) {
+//   try {
+//     await request.jwtVerify();  // Verifies the JWT token in requests
+//   } catch (err) {
+//     reply.send(err);  // Send error if verification fails
+//   }
+// });
 const start = async () => {
     try {
       await fastify.listen({port: 3000,  host:'0.0.0.0'})
