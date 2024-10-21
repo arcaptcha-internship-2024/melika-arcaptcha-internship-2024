e2093993ecfb483f3a9674523cc9f216d688e9cd
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-21 14:36:08 +0330

    chore: register new path for registering user

--- projects/backend/routes/routes.js
+++ projects/backend/routes/routes.js
@@ -1,10 +1,14 @@
 const {saveUserData} = require('../controllers/operations')
 const {login} = require('../controllers/operations')
+const {registerUser} = require('../controllers/operations')
 function routes(fastify,options,done){
     fastify.register(require('@fastify/formbody'));
     fastify.post('/upload',saveUserData)
     fastify.post('/login', async (request, reply) => {
         await login(fastify, request, reply);
+    });
+    fastify.post('/registerUser', async (request, reply) => {
+        await registerUser(fastify, request, reply);
     });
     done()
 }
