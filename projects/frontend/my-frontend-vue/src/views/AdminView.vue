3df0719aa898ce3719e38ce9d58a5779329cfec6
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-22 18:04:10 +0330

    chore: add two button to display list of users and sales managers

--- projects/frontend/my-frontend-vue/src/views/AdminView.vue
+++ projects/frontend/my-frontend-vue/src/views/AdminView.vue
@@ -3,13 +3,28 @@
     <router-link :to="{name: 'registerUser'}" class="registerUser-link">
       <button class="registerUser-button">Register User</button>
     </router-link>
+    <button class="toggle-button">Users</button>
+    <button class="toggle-button" @click="toggleShowSalesManager">Sales Managers</button>
   </nav>
-  <h1>Welcome to the Admin Panel!</h1>
+  <div v-if="showSalesManagers">
+    <UserCardList />
+  </div>
 </template>
 
 <script>
+import {ref, watch} from 'vue'
+import UserCardList from '../components/UserCardList.vue'
+import UserCard from '../components/UserCard.vue'
 export default {
-
+  components: {UserCardList, UserCard},
+  setup(){
+    const showSalesManagers = ref(false)
+    const toggleShowSalesManager = () => {
+      showSalesManagers.value = !showSalesManagers.value
+    }
+    
+    return{showSalesManagers,toggleShowSalesManager}
+  }
 }
 </script>
 
@@ -18,11 +33,15 @@ nav{
   display: flex;
 
 }
-.registerUser-link {
+.registerUser-link, .toggle-button{
   display: inline-block;
   width: 10%;
 }
 .registerUser-button{
   width: 100%;
+}
+.toggle-button{
+  width: 40%;
+  margin: 0 10px 0 10px
 }
 </style>
