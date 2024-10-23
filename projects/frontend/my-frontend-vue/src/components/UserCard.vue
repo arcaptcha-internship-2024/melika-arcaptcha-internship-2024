6933cf33806657ca0d34512e47234e99ae134a64
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-23 14:04:33 +0330

    feat: link read button to userDetails and pass id

--- projects/frontend/my-frontend-vue/src/components/UserCard.vue
+++ projects/frontend/my-frontend-vue/src/components/UserCard.vue
@@ -6,7 +6,7 @@
     <h1>{{user.name}}</h1>
     <p>{{user.companyName}}</p>
     <div class="actions">
-        <router-link class="no-style">
+        <router-link class="no-style" :to="{name: 'userDetail', params: {id: user.id}}">
             <button class="action-button">Read</button>
         </router-link>
         <button class="action-button" v-if="deleteAccess">Delete</button>
