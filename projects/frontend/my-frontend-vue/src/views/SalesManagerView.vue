120a099ea4d2689edc5c2d86b58444f15e8f58ea
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-23 20:03:52 +0330

    feat: display list of users for sales manager

--- projects/frontend/my-frontend-vue/src/views/SalesManagerView.vue
+++ projects/frontend/my-frontend-vue/src/views/SalesManagerView.vue
@@ -1,13 +1,66 @@
 <template>
-  <h1>Welcome to the Sales Manager Panel!</h1>
+  <nav class="nav-container">
+    <router-link>
+      <button class="register-button">View Logs</button>
+    </router-link>
+    <router-link>
+      <button class="logout-button">Log Out</button>
+    </router-link>
+  </nav>
+  <div class="create">
+    <router-link class="no-style" :to="{name: 'create'}">
+      <button class="create-button">Create</button> 
+    </router-link>
+  </div>
+  <div>
+    <UserCardList :path="usersPath" :isSalesManager="false" :role="role"/>
+  </div>
 </template>
 
-<script>
+<script> 
+import {ref, watch} from 'vue'
+import UserCardList from '../components/UserCardList.vue'
+import UserCard from '../components/UserCard.vue'
 export default {
-
+  components: {UserCardList, UserCard},
+  setup(){
+    const role = ref('sales_manager')
+    const showUsers = ref(false)
+    const usersPath = ref('http://localhost:3000/getUsers')
+    const toggle = () => {
+      showUsers.value = !showUsers.value
+    }
+    
+    return{toggle,showUsers, usersPath, role}
+  }
 }
 </script>
 
 <style>
+nav{
+  display: flex;
 
+}
+.registerUser-link, .toggle-button{
+  display: inline-block;
+  width: 10%;
+}
+.registerUser-button, .logout-button{
+  width: 100%;
+}
+.toggle-button{
+  width: 35%;
+  margin: 0 10px 0 10px
+}
+.logout-button{
+  margin-left: 10px;
+}
+.create{
+  margin-top: 10px;
+  display: flex;
+}
+.create-button{
+  display: inline-block;
+  width: 10%;
+}
 </style>
