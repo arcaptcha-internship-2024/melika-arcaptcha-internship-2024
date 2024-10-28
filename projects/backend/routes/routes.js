ce7be862775f5ee3b421561a9c37cdea87393dd2
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-28 16:34:01 +0330

    chore: register downloadUsersList route

--- projects/backend/routes/routes.js
+++ projects/backend/routes/routes.js
@@ -1,9 +1,10 @@
-const {saveUserData} = require('../controllers/operations')
+const {saveUserData, downloadUsers} = require('../controllers/operations')
 const {login} = require('../controllers/operations')
 const {registerUser} = require('../controllers/operations')
 const {getUsers} = require('../controllers/operations')
 const {updateUser} = require('../controllers/operations')
 const {deleteUser} = require('../controllers/operations')
+const fs = require('fs');
 
 function routes(fastify,options,done){
     fastify.register(require('@fastify/formbody'));
@@ -23,6 +24,29 @@ function routes(fastify,options,done){
     });
     fastify.post('/updateUser',updateUser)
     fastify.delete('/deleteUser', deleteUser)
+    // fastify.get('/downloadUsersList', async (request,reply) =>{
+    //     await downloadUsers(request, reply)
+    // })
+    fastify.get('/downloadUsersList', async (request, reply) => {
+        try {
+            const filePath = './database/user.json';
+    
+            // Check if the file exists
+            if (fs.existsSync(filePath)) {
+                console.log("OKKKKKKKKKKKKKKKKKK")
+                reply.header('Content-Disposition', 'attachment; filename="user.json"');
+                reply.header('Content-Type', 'application/json');
+                return reply.send(fs.createReadStream(filePath));
+            } else {
+                console.log("Not founddd!!!!!!!!!!!!!")
+                reply.code(404).send({ error: 'File not found' });
+            }
+        } catch (error) {
+            console.log("!!!!!!!!!!!!!!!!!!!!!",error,"!!!!!!!!!!!!!!!!!!!!!!")
+            request.log.error(error);
+            reply.code(500).send({ error: 'Internal Server Error' });
+        }
+    });
     done()
 }
 
