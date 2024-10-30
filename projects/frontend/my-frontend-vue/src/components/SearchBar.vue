91ff5b914b0020520eac97cae0b26aeeaa2fcbdd
melikaBagherii <m.bagheri1029@gmail.com>
2024-10-30 12:27:42 +0330

    feat: create search bar

---
+++ projects/frontend/my-frontend-vue/src/components/SearchBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="search-bar">
+      <input
+        type="text" 
+        v-model="filters.name"
+        @input="emitFilters"
+        placeholder="Search by name" 
+      />
+      
+      <input
+        type="text"
+        v-model="filters.phoneNumber"
+        @input="emitFilters"
+        placeholder="Search by phone number" 
+      />
+
+      <select @change="emitFilters" v-model="filters.status">
+        <option value="">All Statuses</option>
+        <option value="Pending">Pending</option>
+        <option value="Approved">Approved</option>
+        <option value="Rejected">Rejected</option>
+      </select>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    setup(_,{emit}){
+        const filters = ref({
+            name: "",
+            phoneNumber: "",
+            status: ""
+        })
+        const emitFilters = () => {
+            emit("updateFilters", filters.value)
+        }
+        return {filters, emitFilters}
+    }
+}
+</script>
+
+<style>
+.search-bar {
+  display: flex;
+  gap: 10px;
+  margin: 20px 0;
+}
+</style>
