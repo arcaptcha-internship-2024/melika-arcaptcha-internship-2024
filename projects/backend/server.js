947cc9eb2688f2f5e8b10ed483e6d58417c4f0a3
melikaBagherii <m.bagheri1029@gmail.com>
2024-11-04 15:17:19 +0330

    chore: add .env file in parent folder

--- projects/backend/server.js
+++ projects/backend/server.js
@@ -1,8 +1,12 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
 const fastify = require('fastify')({
     logger:true,
     bodyLimit: 100000
 });
-const path = require('path');
+
+const { env } = require('process');
 fastify.register(require('./routes/routes'))
 // fastify.register(require('@fastify/static'), {
 //     root: path.join(__dirname, '../frontend'),
@@ -14,7 +18,7 @@ fastify.register(require('@fastify/cors'), {
   origin: '*'
 });
 require("./hooks/jwt.js")(fastify);
-require('dotenv').config();
+
 fastify.register(require('@fastify/formbody'));
 fastify.register(require('@fastify/jwt'), {
   secret: process.env.SECRET_KEY
