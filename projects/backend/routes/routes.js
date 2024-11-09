7f31e9daaae03de9117cfdbe5990ceadd4d5be2a
melikaBagherii <m.bagheri1029@gmail.com>
2024-11-09 14:40:39 +0330

    chore: remove getSalesManager route and handle it using getUsers

--- projects/backend/routes/routes.js
+++ projects/backend/routes/routes.js
@@ -18,37 +18,18 @@ function routes(fastify,options,done){
         await registerUser(fastify, request, reply);
     });
 
-    fastify.get('/getSalesManagers', async (request, reply) => {
-        await getUsers('./database/users.json', request, reply);
-    });
+    // fastify.get('/getSalesManagers', async (request, reply) => {
+    //     await getUsers('./database/users.json', request, reply);
+    // });
     fastify.get('/getUsers', async (request, reply) => {
-        await getUsers('./database/customers.json', request, reply);
+        await getUsers(request, reply);
     });
     fastify.post('/updateUser',updateUser)
     fastify.delete('/deleteUser', deleteUser)
     // fastify.get('/downloadUsersList', async (request,reply) =>{
     //     await downloadUsers(request, reply)
     // })
-    fastify.get('/downloadUsersList', async (request, reply) => {
-        try {
-            const filePath = './database/customers.json';
-    
-            // Check if the file exists
-            if (fs.existsSync(filePath)) {
-                console.log("OKKKKKKKKKKKKKKKKKK")
-                reply.header('Content-Disposition', 'attachment; filename="customers.json"');
-                reply.header('Content-Type', 'application/json');
-                return reply.send(fs.createReadStream(filePath));
-            } else {
-                console.log("Not founddd!!!!!!!!!!!!!")
-                reply.code(404).send({ error: 'File not found' });
-            }
-        } catch (error) {
-            console.log("!!!!!!!!!!!!!!!!!!!!!",error,"!!!!!!!!!!!!!!!!!!!!!!")
-            request.log.error(error);
-            reply.code(500).send({ error: 'Internal Server Error' });
-        }
-    });
+    fastify.get('/downloadUsersList', downloadUsers);
     fastify.post('/addLogs',addLog)
     fastify.get('/getLogs', async (request, reply) => {
         await getUsers('./database/logs.json', request, reply);
