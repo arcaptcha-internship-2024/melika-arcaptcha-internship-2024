962dab1f8ec019fe9c6ae3e813f71fabe0a63c2b
melikaBagherii <m.bagheri1029@gmail.com>
2024-11-17 16:28:27 +0330

    feat: add a hook for captcha validation

--- projects/backend/hooks/jwt.js
+++ projects/backend/hooks/jwt.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 module.exports = (fastify) => {
 	fastify.addHook("preHandler", async (req, res) => {
 		if (req.url != "/api/login" && req.url != "/api/customer/submit") {
@@ -9,6 +11,28 @@ module.exports = (fastify) => {
 				.catch(() => {
 					return res.code(401).send({ message: "Unauthorized" });
 				});
+		}
+	});
+
+	fastify.addHook("preValidation", async (req, res) => {
+		if (req.url === "/api/login" || req.url === "/api/customer/submit") {
+			const {'arcaptcha-token':captcha_token} = req.body
+			const captcha_api = process.env.CAPTCHA_API;
+			try {
+				const result = await axios.post(captcha_api, {
+					challenge_id: captcha_token,
+					site_key: process.env.SITE_KEY,
+					secret_key: process.env.SECRET_KEY,
+				})
+				if(result.data.success){
+					console.log('captcha Solved Correctly')
+				}
+				else{
+					return res.code(401).send({ message: "Verify you are a Human!" });
+				}
+			} catch (error) {
+				return res.send(error)
+			}
 		}
 	});
 };
