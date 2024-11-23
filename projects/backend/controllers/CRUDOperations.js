ccee0b01a2be898da18b0c56f5eff3450120aa3c
melikaBagherii <m.bagheri1029@gmail.com>
2024-11-23 05:39:06 +0330

    chore: connect to postgres using docker

--- projects/backend/controllers/CRUDOperations.js
+++ projects/backend/controllers/CRUDOperations.js
@@ -1,22 +1,23 @@
 const fs = require('fs').promises;
+const { create } = require('domain');
 const {Client} = require('pg')
+const myDatabase = require('../database/database.js');
+const { table } = require('console');
 
 async function createDBConnection() {
     const myDatabase = new Client({
         host: "localhost",
         user: "postgres",
-        port: 5432,
-        password: process.env.DATABASE_PASSWORD,
-        database: "arcaptchaInternshipProject",
+        password: process.env.DATABASE_PASSWORD  
     })
     return myDatabase
 }
 
 
 async function read(table){
-    const myDatabase = await createDBConnection()
+    // const myDatabase = await createDBConnection()
     try {
-        await myDatabase.connect();
+        // await myDatabase.connect();
         const res = await myDatabase.query(`SELECT * FROM ${table}`);
         databaseArray = res.rows;
         return databaseArray;
@@ -24,15 +25,15 @@ async function read(table){
         console.error('Database query error:', err.message);
         throw err;
     } finally {
-        await myDatabase.end();
+        // await myDatabase.end();
     }
 }
 
 
 async function update(table, userData) {
-    const myDatabase = await createDBConnection()
+    // const myDatabase = await createDBConnection()
     try {
-        await myDatabase.connect();
+        // await myDatabase.connect();
         if(table === 'customers'){
             const query = `
                 UPDATE ${table}
@@ -84,7 +85,7 @@ async function update(table, userData) {
         console.error('Database query error:', err.message);
         throw err;
     } finally {
-        await myDatabase.end();
+        // await myDatabase.end();
     }
 }
 
@@ -92,17 +93,10 @@ async function update(table, userData) {
 async function write(userData, filePath) {
     console.log('this is user data: ',userData)
     console.log('this is table name: ', filePath)
-    const {Client} = require('pg')
-    const myDatabase = new Client({
-        host: "localhost",
-        user: "postgres",
-        port: 5432,
-        password: "thisisKamelika13",
-        database: "arcaptchaInternshipProject",
-    })
+    // const myDatabase = await createDBConnection()
     
     try {
-        await myDatabase.connect();
+        // await myDatabase.connect();
         if(filePath === 'customers'){
             const query = `
                 INSERT INTO ${filePath} ("id", "name", "companyName", "jobPosition", "phoneNumber", "explanation", "createdDate", "lastUpdate", "status", "supervisorExplanation")
@@ -155,34 +149,87 @@ async function write(userData, filePath) {
         console.error('Error inserting data into database:', err.message);
         throw err;
     } finally {
-        await myDatabase.end();
+        // await myDatabase.end();
     }
 
+}
+
+const createTable = async () => {
+    // const myDatabase = new Client({
+    //     host: "db", // Use the service name from Docker Compose
+    //     user: "postgres",
+    //     password: "thisisKamelika13",
+    //     database: "postgres", // Default database in PostgreSQL
+    //     port: 5432, // PostgreSQL default port
+    // });
+
+    try {
+        console.log('trying to connect to database')
+        // await myDatabase.connect();
+        console.log('connected!')
+        await myDatabase.query(`DROP TABLE IF EXISTS users`)
+        await myDatabase.query(`DROP TABLE IF EXISTS customers`)
+        await myDatabase.query(`DROP TABLE IF EXISTS logs`)
+
+
+        await myDatabase.query('CREATE TABLE IF NOT EXISTS users ("id" text PRIMARY KEY, "email" text, "password" text, "role" text)')
+        console.log('users database Created!')
+        await myDatabase.query('CREATE TABLE IF NOT EXISTS customers ("id" text PRIMARY KEY, "name" text, "companyName" text, "jobPosition" text, "phoneNumber" text, "explanation" text, "createdDate" text, "lastUpdate" text, "status" text, "supervisorExplanation" text)')
+        console.log('customers database Created!')
+        await myDatabase.query('CREATE TABLE IF NOT EXISTS logs ("log" text)')
+        console.log('logs database Created!')
+        await myDatabase.query(`
+            INSERT INTO users ("id", "email", "password", "role")
+            SELECT '1', 'melika@gmail.com', '12345m', 'admin'
+            WHERE NOT EXISTS (SELECT 1 FROM users);
+        `);
+
+        console.log('Data inserted!');
+
+        // Retrieve the data to send as the response
+        const result = await myDatabase.query('SELECT * FROM users');
+        console.log('Data fetched!');
+        reply.send(result.rows)
+    } catch (error) {
+        console.log('error: ',error)
+    }
+}
+
+const getTest = async (request, reply) => {
+    console.log(request.body)
+    const result = await myDatabase.query('SELECT * FROM customers');
+    reply.send(result.rows)
 }
 
 
 const downloadUsers = async (request,reply) => {
     const fs = require('fs');
+    const path = require('path');
+    console.log('hiiiiiii')
     try {
-        const filePath = request.headers['x-file-path']
-
-        if (fs.existsSync(filePath)) {
-            reply.header('Content-Disposition', 'attachment; filename="customers.json"');
-            reply.header('Content-Type', 'application/json');
-            return reply.send(fs.createReadStream(filePath));
-        } else {
-            reply.code(404).send({ error: 'File not found' });
-        }
+        const tableName = request.headers['x-file-path']
+        const result = await myDatabase.query(`SELECT * FROM ${tableName}`);
+        const tableData = result.rows;
+        console.log(tableData)
+        const jsonData = JSON.stringify(tableData, null, 2);
+        console.log(jsonData)
+        reply.header('Content-Disposition', `attachment; filename=${tableName}.json`);
+        reply.header('Content-Type', 'application/json');
+        reply.send(jsonData)
     } catch (error) {
-        request.log.error(error);
-        reply.code(500).send({ error: 'Internal Server Error' });
+        console.error('Error exporting data:', error.message);
+        reply.status(500).send({ error: 'Failed to export data' });
     }
+    
 }
+
 
 
 module.exports = {
     read,
     write,
     downloadUsers,
-    update
+    update,
+    createTable,
+    getTest
 };
